3108361fd0c16456347acac4e259ad7a6b5f4865
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2015-01-12 16:58:56 +0100

    import edited

--- src/main/java/baladeva/Main.java
+++ src/main/java/baladeva/Main.java
@@ -1,41 +1,12 @@
 package baladeva;
 
-import java.awt.Color;
-import java.awt.Composite;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GraphicsConfiguration;
-import java.awt.Image;
-import java.awt.Paint;
-import java.awt.Rectangle;
-import java.awt.RenderingHints;
-import java.awt.RenderingHints.Key;
-import java.awt.Shape;
-import java.awt.Stroke;
-import java.awt.font.FontRenderContext;
-import java.awt.font.GlyphVector;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
-import java.awt.image.BufferedImageOp;
-import java.awt.image.ImageObserver;
-import java.awt.image.RenderedImage;
-import java.awt.image.renderable.RenderableImage;
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.text.AttributedCharacterIterator;
-import java.util.Map;
-
 import gameframework.base.ObservableValue;
-import gameframework.drawing.BackgroundImage;
-import gameframework.drawing.DrawableImage;
 import gameframework.drawing.GameCanvas;
 import gameframework.drawing.GameCanvasDefaultImpl;
-import gameframework.drawing.MockGraphics;
 import gameframework.game.GameConfiguration;
 import gameframework.gui.GameWindow;
+
+import java.net.MalformedURLException;
 
 public class Main {
 
