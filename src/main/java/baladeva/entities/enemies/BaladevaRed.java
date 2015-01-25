5ef9c693e05e39f3f8df12f4f179f07c2583f5f8
T-Brawl <t-brawl@hotmail.fr>
2015-01-26 00:19:55 +0100

    New enemy

---
+++ src/main/java/baladeva/entities/enemies/BaladevaRed.java
@@ -0,0 +1,26 @@
+package baladeva.entities.enemies;
+
+import gameframework.game.GameData;
+import java.awt.Point;
+import baladeva.entities.BaladevaPlayer;
+
+public class BaladevaRed extends BaladevaWolf {
+
+	public BaladevaRed(GameData data, int x, int y, BaladevaPlayer player) {
+		super(data, x, y, player);
+		this.hitPoints = 12;
+		this.scorePoints = 2000;
+	}
+	
+	public BaladevaRed(GameData data, Point pos, Point goal) {
+		super(data, pos, goal);
+		this.hitPoints = 12;
+		this.scorePoints = 2000;
+	}	
+
+	@Override
+	protected String imageStr() {
+		return "/images/level1/red.png";
+	}
+
+}
