4cbd7c59b44e7f9180ef63b6944782d5883f8009
Bafou <antoine-59630@hotmail.fr>
2015-01-26 12:19:29 +0100

    Comment the different enemies entity

--- src/main/java/baladeva/entities/enemies/BaladevaKnight.java
+++ src/main/java/baladeva/entities/enemies/BaladevaKnight.java
@@ -8,8 +8,20 @@ import java.awt.Point;
 
 import baladeva.entities.BaladevaEnemy;
 
+/**
+ * BaladevaKnight represent the enemy who looks like a knight. Its moveStrategy is a StraightLineStragy in order to
+ * follow the player.
+ * @author WISSOCQ Sarah, AGEZ Adrien, COJEZ Arnaud, MOEVI Alexandre, PETIT Antoine
+ *
+ */
 public class BaladevaKnight extends BaladevaEnemy {
 	
+	/**
+	 * Create an enemy knight.
+	 * @param data the data of the current game.
+	 * @param pos the position where the enemy will be at the start of the game.
+	 * @param goal the goal of the enemy.
+	 */
 	public BaladevaKnight(GameData data, Point pos, Point goal) {
 		super(data, pos, goal);
 		this.hitPoints = 18;
